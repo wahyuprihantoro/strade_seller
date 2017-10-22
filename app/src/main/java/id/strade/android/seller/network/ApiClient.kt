819506e829f475f382e6c92c6f567db2b2020d45
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-10-22 13:46:20 +0700

    call api service backend

--- app/src/main/java/id/strade/android/seller/network/ApiClient.kt
+++ app/src/main/java/id/strade/android/seller/network/ApiClient.kt
@@ -1,8 +1,12 @@
 package id.strade.android.seller.network
 
+import android.util.Log
 import org.androidannotations.annotations.EBean
+import org.json.JSONObject
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 /**
  * Created by wahyu on 19 October 2017.
@@ -10,7 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @EBean
 open class ApiClient {
 
-    val BASE_URL: String = "localhost:8000/api"
+    val BASE_URL: String = "http://159.89.200.247/api/"
 
     fun <T> getService(service: Class<T>): T {
         return getRetrofit().create(service)
@@ -20,5 +24,28 @@ open class ApiClient {
         return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create()).build()
+    }
+
+    fun login(username:String,password:String,role:String){
+        var response:JSONObject
+        val retrofit = getRetrofit()
+        val loginService = retrofit.create(LoginService::class.java)
+        val call = loginService.login(username,password,role)
+        var body: Response<JSONObject>? = null
+        try {
+            body = call.execute()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        try{
+            response = body!!.body()!!
+            val isSuccess = Integer.toString(body!!.code())
+            Log.i("SUCCESS", isSuccess)
+            Log.i("BODY", body!!.message())
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+
     }
 }
