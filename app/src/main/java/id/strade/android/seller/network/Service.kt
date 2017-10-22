9b9298e68471e656724a9c7281dd79f7f9b4aae7
Muhammad Ghozi <ghozi.mghozi@gmail.com>
2017-10-22 13:45:53 +0700

    bikin service interface retrofit

--- app/src/main/java/id/strade/android/seller/network/Service.kt
+++ app/src/main/java/id/strade/android/seller/network/Service.kt
@@ -1,5 +1,21 @@
 package id.strade.android.seller.network
 
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+
+
+
 /**
  * Created by ARSnova on 22/10/2017.
  */
+interface LoginService {
+    @FormUrlEncoded
+    @POST("login")
+    fun login(@Field("username") username: String,
+              @Field("password") password: String,
+              @Field("role") role: String): Call<JSONObject>
+}
