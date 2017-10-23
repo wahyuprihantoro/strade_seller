8db81bacd7db9cffc169810aff3788074be77d86
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-10-23 15:17:38 +0700

    profile

--- app/src/main/java/id/strade/android/seller/homefragment/AccountFragment.kt
+++ app/src/main/java/id/strade/android/seller/homefragment/AccountFragment.kt
@@ -1,16 +1,39 @@
 package id.strade.android.seller.homefragment
 
 import android.support.v4.app.Fragment
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import id.strade.android.seller.R
+import id.strade.android.seller.storage.Prefs
 import org.androidannotations.annotations.AfterViews
+import org.androidannotations.annotations.Bean
 import org.androidannotations.annotations.EFragment
+import org.androidannotations.annotations.ViewById
+
 
 @EFragment(R.layout.fragment_account)
 open class AccountFragment : Fragment() {
 
+    @Bean
+    lateinit var prefs: Prefs
+    @ViewById(R.id.photo_profile)
+    lateinit var profilePictureImageView: ImageView
+    @ViewById(R.id.cover)
+    lateinit var coverImageView: ImageView
+    @ViewById(R.id.name)
+    lateinit var nameView: TextView
+
     @AfterViews
     fun init() {
-
+        val user = prefs.user
+        Glide.with(this)
+                .load(user.imageUrl)
+                .into(profilePictureImageView)
+        Glide.with(this)
+                .load(user.store?.imageUrl)
+                .into(coverImageView)
+        nameView.text = user.fullName
     }
 
 }
