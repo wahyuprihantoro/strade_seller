20a972a18cc3b67c694bce32d16082874f37fc03
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-11-09 07:07:01 +0700

    add google maps to home page

--- app/src/main/java/id/strade/android/seller/homefragment/MapFragment.kt
+++ app/src/main/java/id/strade/android/seller/homefragment/MapFragment.kt
@@ -1,16 +1,34 @@
 package id.strade.android.seller.homefragment
 
 import android.support.v4.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import id.strade.android.seller.R
 import org.androidannotations.annotations.AfterViews
 import org.androidannotations.annotations.EFragment
 
 @EFragment(R.layout.fragment_map)
-open class MapFragment : Fragment() {
+open class MapFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
 
     @AfterViews
     fun init() {
+        val mapFragment = activity.supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }
