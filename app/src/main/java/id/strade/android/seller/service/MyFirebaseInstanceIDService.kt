cb8e1ce3c91f19312690c1242e1ca15755314869
Wahyu Prihantoro <wahyuprihantoro21@gmail.com>
2017-11-27 00:06:26 +0700

    setup firebase cloud notification

---
+++ app/src/main/java/id/strade/android/seller/service/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,9 @@
+package id.strade.android.seller.service
+
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+/**
+ * Created by wahyu on 27 November 2017.
+ */
+class MyFirebaseInstanceIDService: FirebaseInstanceIdService() {
+}
---
+++ app/src/main/java/id/strade/android/seller/service/MyFirebaseMessagingService.kt
@@ -0,0 +1,8 @@
+package id.strade.android.seller.service
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+/**
+ * Created by wahyu on 26 November 2017.
+ */
+class MyFirebaseMessagingService : FirebaseMessagingService()
